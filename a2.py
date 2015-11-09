a90b718783276cf63fd1af39ebfda229dcd131a5
Thakdanai Khunsaen <a5810059@gmail.com>
2015-11-09 14:44:03 +0000

    Add & Remove Frame

--- a2.py
+++ a2.py
@@ -3,6 +3,7 @@ class Amaker:
         self.width = width
         self.height = height
         self.size = []
+        self.frame = []
         for i in range(0,self.height):
             self.size.append([" "]*width)
         
@@ -17,13 +18,18 @@ class Amaker:
         self.size[y2][x2] = "#"
     
     def add_frame(self):  # add a frame after current frame
-        pass
-
+        currentFrame = []
+        for i in range(0,self.height):
+            currentFrame.append([" "]*self.width)
+            for j in range(0,len(self.size[i])):
+                currentFrame[i][j] = self.size[i][j]
+        self.frame.append(currentFrame)
+        print("!Add Frame!")
     def remove_frame(self): # remove current frame (must be the last frame?)
-        pass
-
+        del self.frame[-1]
+        print("!Remove Frame!")
     def get_total_number_of_frames(self):
-        pass
+        print("Total Frame =",len(self.frame))
 
     def display_current_frame(self):  # print out the frame in text-mode
         for i in range(0,len(self.size)):
@@ -31,10 +37,14 @@ class Amaker:
                 print(self.size[i][j],end = "")
             print()
         print("------------------")
+    
     def get_total_number_of_tiles(self):
-        pass
-
-
+        count = 0
+        for i in range(0,len(self.size)):
+            for j in range(0,len(self.size[i])):
+                if(self.size[i][j] == "#"):
+                    count += 1
+        print("total number of tiles =",count)
 
 def main():
     a = Amaker(18, 10)
@@ -43,18 +53,21 @@ def main():
     a.add_tile(6, 1)
     a.add_tile(10, 1)
     a.add_tile(4, 3)
-
     a.display_current_frame()
-    a.remove_tile(10, 1)
-
-    a.display_current_frame()
-
     a.add_frame()
-
-    a.move_tile(6, 1, 6, 2)
-
+    
+    a.remove_tile(10, 1)
     a.display_current_frame()
+    a.add_frame()
+    
+    a.move_tile(6, 1, 6, 2)
+    a.display_current_frame()
+    a.add_frame()
+    
+    a.remove_frame()
 
+    a.get_total_number_of_tiles()
+    a.get_total_number_of_frames()
     print('Exit')
 
 
