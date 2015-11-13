6ff4c40a65a88d55db82fb1e149ce11326839c10
Thakdanai Khunsaen <a5810059@gmail.com>
2015-11-13 16:12:13 +0000

    #text mode - Add / Remove Frame

--- a2.java
+++ a2.java
@@ -1,13 +1,32 @@
 class Amaker {
   int widthX, heightY;
   String size[][];
+  int frameNum;
+  String frame[][][];
   Amaker(int w, int h) {
     widthX = w;
     heightY = h;
     size = new String[heightY][widthX];
+    frameNum = 0;
+    frame = new String[100][heightY][widthX];
+  }
+  int get_w() {
+    return widthX;
+  }
+  int get_h() {
+    return heightY;
   }
   String[][] get_size() {
     return size;
+  }
+  String[][][] get_frame() {
+    return frame;
+  }
+  int get_frameNum() {
+    return frameNum;
+  }
+  void setFrame(int set) {
+    frameNum+=set;
   }
 }
 class Acontroller {
@@ -25,9 +44,22 @@ class Acontroller {
     a.get_size()[y1][x1] = " ";
     a.get_size()[y2][x2] = "#";
   }
+  void add_frame() {
+    a.setFrame(1);
+    for (int i = 0; i<a.get_size().length; i++) {
+      for (int j = 0; j<a.get_size()[i].length; j++) {
+        a.get_frame()[a.get_frameNum()-1][i][j] = a.get_size()[i][j];
+      }
+    }
+    println("!Add Frame!");
+  }
+  void remove_frame() {
+    a.setFrame(-1); 
+    println("!Remove Frame!");
+  }
 }
 class Aviewer {
-  Amaker a;
+  Amaker a; 
   Aviewer(Amaker b) {
     a = b;
   }
@@ -42,28 +74,45 @@ class Aviewer {
       }
       println();
     }
-    print("------------------");
+    println("------------------");
+  }
+  void display_all_frame() {
+    println("\nDisplay All Frame");
+    for (int k = 0; k<a.get_frameNum(); k++) {
+      for (int i = 0; i<a.get_size().length; i++) {
+        for (int j = 0; j<a.get_size()[i].length; j++) {
+          if (a.get_frame()[k][i][j] == null) {
+            print(" ");
+          } else {
+            print(a.get_frame()[k][i][j]);
+          }
+        }
+        println();
+      }
+      println("------------------");
+    }
   }
 }
 void setup() {
-  Amaker a = new Amaker(18, 10);
-  Acontroller b = new Acontroller(a);
-  Aviewer c = new Aviewer(a);
-  
-  b.add_tile(4, 1);
-  b.add_tile(6, 1);
-  b.add_tile(10, 1);
-  b.add_tile(4, 3);
-  c.display_current_frame();
-  //a.add_frame()
+  Amaker a = new Amaker(18, 10); 
+  Acontroller b = new Acontroller(a); 
+  Aviewer c = new Aviewer(a); 
 
-  b.remove_tile(10, 1);
-  c.display_current_frame();
-  //a.add_frame();
+  b.add_tile(4, 1); 
+  b.add_tile(6, 1); 
+  b.add_tile(10, 1); 
+  b.add_tile(4, 3); 
+  c.display_current_frame(); 
+  b.add_frame(); 
 
-  b.move_tile(6, 1, 6, 2);
-  c.display_current_frame();
-  //a.add_frame();
+  b.remove_tile(10, 1); 
+  c.display_current_frame(); 
+  b.add_frame(); 
 
-  //a.remove_frame();
+  b.move_tile(6, 1, 6, 2); 
+  c.display_current_frame(); 
+  b.add_frame(); 
+  c.display_all_frame(); 
+  b.remove_frame(); 
+  c.display_all_frame();
 }
